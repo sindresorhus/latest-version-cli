86081e44c630f8c14d9ec62f8b24c0a09bd7be72
LitoMore <litomore@gmail.com>
2019-12-08 00:07:21 +0800

    Add support for specifying the dist-tag

--- test.js
+++ test.js
@@ -2,7 +2,12 @@ import test from 'ava';
 import execa from 'execa';
 import semverRegex from 'semver-regex';
 
-test(async t => {
+test('test without flags', async t => {
 	const {stdout} = await execa('./cli.js', ['ava']);
 	t.regex(stdout, semverRegex());
+});
+
+test('test with flags', async t => {
+	const {stdout} = await execa('./cli.js', ['update-notifier-tester', '--version=0.0.3-rc1']);
+	t.is(stdout, '0.0.3-rc1');
 });
