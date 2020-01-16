a76a8419a16cb6cbd0cdec6509feca02099c5bac
Sindre Sorhus <sindresorhus@gmail.com>
2020-01-16 22:47:35 +0700

    Update cli.js

--- cli.js
+++ cli.js
@@ -5,18 +5,18 @@ const latestVersion = require('latest-version');
 
 const cli = meow(`
 	Usage
-		$ latest-version <package-name> [options...]
+	  $ latest-version <package-name>
 
 	Options
-		-v       	Ouput current version number of this package
-		--version	Specify which dist-tag to use to find the latest version
+	  -v         Ouput the version of this package
+	  --version  Specify which dist-tag to use to find the latest version
 
 	Example
 	  $ latest-version ava
-		2.4.0
+	  2.4.0
 
-		$ latest-version ava --version=next
-		2.0.0-rc.1
+	  $ latest-version ava --version=next
+	  2.0.0-rc.1
 `, {
 	autoVersion: false
 });
