9716dab2d268a010a4121f00b7b1b1c2f7d27831
Sindre Sorhus <sindresorhus@gmail.com>
2020-01-16 22:49:39 +0700

    Update test.js

--- test.js
+++ test.js
@@ -2,12 +2,12 @@ import test from 'ava';
 import execa from 'execa';
 import semverRegex from 'semver-regex';
 
-test('test without flags', async t => {
+test('main', async t => {
 	const {stdout} = await execa('./cli.js', ['ava']);
 	t.regex(stdout, semverRegex());
 });
 
-test('test with flags', async t => {
+test('--version', async t => {
 	const {stdout} = await execa('./cli.js', ['update-notifier-tester', '--version=0.0.3-rc1']);
 	t.is(stdout, '0.0.3-rc1');
 });
